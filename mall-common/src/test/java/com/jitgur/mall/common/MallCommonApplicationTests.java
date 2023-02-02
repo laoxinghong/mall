955f2b798e7d8ff2a2caefdff5bbc76f93b8028c
17806 <17806709603@163.com>
2023-02-02 15:24:22 +0800

    mall-common pom.xml

---
+++ mall-common/src/test/java/com/jitgur/mall/common/MallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitgur.mall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
