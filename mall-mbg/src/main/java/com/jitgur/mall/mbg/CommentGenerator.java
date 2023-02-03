289a057ff2f5c0b2d7e19341b3695f32b02e1185
17806 <17806709603@163.com>
2023-02-03 13:00:31 +0800

    mall-mbg generator config

---
+++ mall-mbg/src/main/java/com/jitgur/mall/mbg/CommentGenerator.java
@@ -0,0 +1,88 @@
+package com.jitgur.mall.mbg;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
+/**
+ * 自定义注解生成器
+ * Create by jitgur on 20230203
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+
+    private boolean addRemarkComments = false;
+    private static final String EXAMPLE_SUFFIX = "example";
+    private static final String MAPPER_SUFFIX = "mapper";
+    private static final String API_MODEL_PROPERTY_FULL_CLASS_NAME = "io.swagger.annotations.ApiModelProperty";
+
+
+    /**
+     * 设置用户配置的参数
+     *
+     * @param properties
+     */
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+
+    /**
+     * 生成@ApiModelProperty注解来替代原来的方法注释
+     *
+     * @param field
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        //根据参数和备注信息判断是否添加备注信息
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            addFieldJavaDoc(field, remarks);
+            //数据库中特殊字符需要转义
+            if (remarks.contains("\"")) {
+                remarks = remarks.replace("\"", "'");
+            }
+            //给model的字段添加swagger注解
+            field.addJavaDocLine("@ApiModelProperty(value = \"" + remarks + "\")");
+        }
+    }
+
+    /**
+     * 给model字段添加注释
+     *
+     * @param field
+     * @param remarks
+     */
+    private void addFieldJavaDoc(Field field, String remarks) {
+        //文档注释开始
+        field.addJavaDocLine("/**");
+        //获取数据库字段的备注信息
+        String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+        for (String remarkLine : remarkLines) {
+            field.addJavaDocLine(" * " + remarkLine);
+        }
+        addJavadocTag(field, false);
+        field.addJavaDocLine(" */");
+    }
+
+    /**
+     * 在import中导入@ApiModelProperty类
+     *
+     * @param compilationUnit
+     */
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        super.addJavaFileComment(compilationUnit);
+        //只在model中添加swagger注解类的导入
+        if (!compilationUnit.getType().getFullyQualifiedName().contains(MAPPER_SUFFIX) && !compilationUnit.getType().getFullyQualifiedName().contains(EXAMPLE_SUFFIX)) {
+            compilationUnit.addImportedType(new FullyQualifiedJavaType(API_MODEL_PROPERTY_FULL_CLASS_NAME));
+        }
+    }
+}
---
+++ mall-mbg/src/test/java/com/jitgur/mall/mbg/MallMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitgur.mall.mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
