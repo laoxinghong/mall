0ae6472630a7508e672a04c235a76d8ae5cda1f0
17806 <17806709603@163.com>
2023-02-04 21:12:53 +0800

    动态权限过滤器，用于实现基于路径的动态权限过滤

---
+++ mall-security/src/main/java/com/jitgur/mall/security/component/DynamicSecurityFilter.java
@@ -0,0 +1,77 @@
+package com.jitgur.mall.security.component;
+
+import com.jitgur.mall.security.config.IgnoreUrlsConfig;
+import io.swagger.models.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.SecurityMetadataSource;
+import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
+import org.springframework.security.access.intercept.InterceptorStatusToken;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 动态权限过滤器，用于实现基于路径的动态权限过滤
+ * Created by jitgur on 20230204
+ */
+public class DynamicSecurityFilter extends AbstractSecurityInterceptor implements Filter {
+
+    @Autowired
+    private DynamicSecurityMetadataSource metadataSource;
+    @Autowired
+    private IgnoreUrlsConfig ignoreUrlsConfig;
+
+    @Autowired
+    public void setMyAccessDecisionManager(DynamicAccessDecisionManager dynamicAccessDecisionManager) {
+        super.setAccessDecisionManager(dynamicAccessDecisionManager);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public Class<?> getSecureObjectClass() {
+        return FilterInvocation.class;
+    }
+
+    @Override
+    public SecurityMetadataSource obtainSecurityMetadataSource() {
+        return metadataSource;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        FilterInvocation filterInvocation = new FilterInvocation(servletRequest, servletResponse, filterChain);
+
+        // options请求直接放行
+        if (httpServletRequest.getMethod().equals(HttpMethod.OPTIONS.toString())) {
+            filterInvocation.getChain().doFilter(filterInvocation.getRequest(), filterInvocation.getResponse());
+            return;
+        }
+
+        // 白名单请求直接放行
+        PathMatcher pathMatcher = new AntPathMatcher();
+        for (String path : ignoreUrlsConfig.getUrls()) {
+            if (pathMatcher.match(path, httpServletRequest.getRequestURI())) {
+                filterInvocation.getChain().doFilter(filterInvocation.getRequest(), filterInvocation.getResponse());
+                return;
+            }
+        }
+
+        // 调用AccessDecisionManager中的decide方法进行鉴权操作
+        InterceptorStatusToken interceptorStatusToken = super.beforeInvocation(filterInvocation);
+        try {
+            filterInvocation.getChain().doFilter(filterInvocation.getRequest(), filterInvocation.getResponse());
+        } finally {
+            super.afterInvocation(interceptorStatusToken, null);
+        }
+    }
+
+}
