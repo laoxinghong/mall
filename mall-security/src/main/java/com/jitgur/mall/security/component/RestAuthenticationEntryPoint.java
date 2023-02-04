112ed56851a4a138b98a4c072653812ff9fc2c9b
17806 <17806709603@163.com>
2023-02-04 15:38:52 +0800

    未登录或token失效时访问接口的自定义返回结果

---
+++ mall-security/src/main/java/com/jitgur/mall/security/component/RestAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.jitgur.mall.security.component;
+
+import cn.hutool.json.JSONUtil;
+import com.jitgur.mall.common.api.CommonResult;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 未登录或token失效时访问接口的自定义返回结果
+ * Created by jitgur on 20230204
+ */
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(authException.getMessage())));
+        response.getWriter().flush();
+    }
+
+}
