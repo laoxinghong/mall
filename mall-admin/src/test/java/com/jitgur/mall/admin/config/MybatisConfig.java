d18c1438bacf8f39774514294d508312b61ae5ae
17806 <17806709603@163.com>
2023-02-05 17:53:20 +0800

    Mybatis相关配置

---
+++ mall-admin/src/test/java/com/jitgur/mall/admin/config/MybatisConfig.java
@@ -0,0 +1,15 @@
+package com.jitgur.mall.admin.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Mybatis相关配置
+ * Created by jitgur on 20230205
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.jitgur.mall.admin.mapper", "com.jitgur.mall.admin.dao"})
+public class MybatisConfig {
+}
