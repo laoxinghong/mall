05ca709e6b967f2b43bd70a788a9fe2241f4bda5
17806 <17806709603@163.com>
2023-02-08 16:28:58 +0800

    mall-security模块相关配置

---
+++ mall-admin/src/main/java/com/jitgur/mall/admin/config/MallSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.jitgur.mall.admin.config;
+
+import com.jitgur.mall.admin.service.UmsAdminService;
+import com.jitgur.mall.admin.service.UmsResourceService;
+import com.jitgur.mall.mbg.model.UmsResource;
+import com.jitgur.mall.security.component.DynamicSecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * mall-security模块相关配置
+ * Created by jitgur on 20230208
+ */
+@Configuration
+public class MallSecurityConfig {
+
+    @Autowired
+    private UmsAdminService adminService;
+    @Autowired
+    private UmsResourceService resourceService;
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> adminService.loadUserByUsername(username);
+    }
+
+
+    @Bean
+    public DynamicSecurityService dynamicSecurityService() {
+        return new DynamicSecurityService() {
+            @Override
+            public Map<String, ConfigAttribute> loadDataSource() {
+                Map<String, ConfigAttribute> map = new HashMap<>();
+                List<UmsResource> resourceList = resourceService.listAll();
+                for (UmsResource resource : resourceList) {
+                    map.put(resource.getUrl(), new SecurityConfig(resource.getId() + ":" + resource.getName()));
+                }
+                return map;
+            }
+        };
+    }
+}
