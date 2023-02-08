42667e9041a72a140fd920d88e726c253ce296b1
17806 <17806709603@163.com>
2023-02-08 16:16:10 +0800

    后台资源管理

---
+++ mall-admin/src/main/java/com/jitgur/mall/admin/service/UmsResourceService.java
@@ -0,0 +1,48 @@
+package com.jitgur.mall.admin.service;
+
+import com.jitgur.mall.mbg.model.UmsResource;
+
+import java.util.List;
+
+/**
+ * 后台资源管理Service
+ * Created by jitgur on 20230208
+ */
+public interface UmsResourceService {
+
+    /**
+     * 添加资源
+     */
+    int create(UmsResource resource);
+
+
+    /**
+     * 修改指定资源
+     */
+    int update(Long resourceId, UmsResource resource);
+
+
+    /**
+     * 删除指定资源
+     */
+    int delete(Long resourceId);
+
+
+    /**
+     * 获取指定资源
+     */
+    UmsResource get(Long resourceId);
+
+
+    /**
+     * 查询全部资源
+     */
+    List<UmsResource> listAll();
+
+
+    /**
+     * 分页查询资源
+     */
+    List<UmsResource> listPage(Long categoryId, String nameKeyword, String urlKeyword, Integer pageNum, Integer pageSize);
+
+}
---
+++ mall-admin/src/main/java/com/jitgur/mall/admin/service/impl/UmsResourceServiceImpl.java
@@ -0,0 +1,83 @@
+package com.jitgur.mall.admin.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import com.github.pagehelper.PageHelper;
+import com.jitgur.mall.admin.service.UmsAdminCacheService;
+import com.jitgur.mall.admin.service.UmsResourceService;
+import com.jitgur.mall.mbg.mapper.UmsResourceMapper;
+import com.jitgur.mall.mbg.model.UmsResource;
+import com.jitgur.mall.mbg.model.UmsResourceExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 后台资源管理Service实现类
+ * Created by jitgur on 20230208
+ */
+@Service
+public class UmsResourceServiceImpl implements UmsResourceService {
+
+    @Autowired
+    private UmsResourceMapper resourceMapper;
+    @Autowired
+    private UmsAdminCacheService adminCacheService;
+
+
+    @Override
+    public int create(UmsResource resource) {
+        resource.setCreateTime(new Date());
+        return resourceMapper.insert(resource);
+    }
+
+
+    @Override
+    public int update(Long resourceId, UmsResource resource) {
+        resource.setId(resourceId);
+        int result = resourceMapper.updateByPrimaryKeySelective(resource);
+        adminCacheService.delResourceListByResourceId(resourceId);
+        return result;
+    }
+
+
+    @Override
+    public int delete(Long resourceId) {
+        int result = resourceMapper.deleteByPrimaryKey(resourceId);
+        adminCacheService.delResourceListByResourceId(resourceId);
+        return result;
+    }
+
+
+    @Override
+    public UmsResource get(Long resourceId) {
+        return resourceMapper.selectByPrimaryKey(resourceId);
+    }
+
+
+    @Override
+    public List<UmsResource> listAll() {
+        UmsResourceExample example = new UmsResourceExample();
+        return resourceMapper.selectByExample(example);
+    }
+
+
+    @Override
+    public List<UmsResource> listPage(Long categoryId, String nameKeyword, String urlKeyword, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        UmsResourceExample example = new UmsResourceExample();
+        UmsResourceExample.Criteria criteria = example.createCriteria();
+        if (categoryId != null) {
+            criteria.andCategoryIdEqualTo(categoryId);
+        }
+        if (StrUtil.isNotEmpty(nameKeyword)) {
+            criteria.andNameLike('%' + nameKeyword + '%');
+        }
+        if (StrUtil.isNotEmpty(urlKeyword)) {
+            criteria.andUrlLike('%' + urlKeyword + '%');
+        }
+        return resourceMapper.selectByExample(example);
+    }
+
+}
