7bbdcb34da27392de74f9222a0bd7bb197c5a35f
17806 <17806709603@163.com>
2023-02-08 15:15:11 +0800

    后台用户资源关系自定义dao

---
+++ mall-admin/src/main/java/com/jitgur/mall/admin/dao/UmsAdminResourceRelationDao.java
@@ -0,0 +1,27 @@
+package com.jitgur.mall.admin.dao;
+
+import com.jitgur.mall.mbg.model.UmsResource;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 用户资源关系dao
+ * Created by jitgur on 20230207
+ */
+@Mapper
+public interface UmsAdminResourceRelationDao {
+
+    /**
+     * 根据资源id获取用户id列表
+     */
+    List<Long> getAdminIdList(@Param("resourceId") Long resourceId);
+
+
+    /**
+     * 根据用户id获取资源列表
+     */
+    List<UmsResource> getResourceList(@Param("adminId") Long adminId);
+
+}
