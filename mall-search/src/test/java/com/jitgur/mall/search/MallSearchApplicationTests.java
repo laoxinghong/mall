43e54c3d794a01a04f3e9ea5b701084725f27e30
17806 <17806709603@163.com>
2023-02-09 14:47:29 +0800

    mall-search

---
+++ mall-search/src/test/java/com/jitgur/mall/search/MallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitgur.mall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
