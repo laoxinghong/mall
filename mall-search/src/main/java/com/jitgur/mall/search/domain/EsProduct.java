0d0d6f34b483d2ad366f68e2ab0b383bc702f1ab
17806 <17806709603@163.com>
2023-02-09 22:12:34 +0800

    es商品信息封装

---
+++ mall-search/src/main/java/com/jitgur/mall/search/domain/EsProduct.java
@@ -0,0 +1,54 @@
+package com.jitgur.mall.search.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 搜索系统商品信息封装
+ * Created by jitgur on 20230209
+ */
+@Data
+@EqualsAndHashCode
+@Document(indexName = "pms")
+@Setting(shards = 1, replicas = 0)
+public class EsProduct implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+
+    @Id
+    private Long id;
+    @Field(type = FieldType.Keyword)
+    private String productSn;
+    private Long brandId;
+    @Field(type = FieldType.Keyword)
+    private String brandName;
+    private Long productCategoryId;
+    @Field(type = FieldType.Keyword)
+    private String productCategoryName;
+    private String pic;
+    @Field(analyzer = "ik_max_word", type = FieldType.Text)
+    private String name;
+    @Field(analyzer = "ik_max_word", type = FieldType.Text)
+    private String subTitle;
+    @Field(analyzer = "ik_max_word", type = FieldType.Text)
+    private String keywords;
+    private BigDecimal price;
+    private Integer sale;
+    private Integer newStatus;
+    private Integer recommendStatus;
+    private Integer stock;
+    private Integer promotionType;
+    private Integer sort;
+    @Field(type = FieldType.Nested)
+    private List<EsProductAttributeValue> attrValueList;
+
+}
