729513f7e9c903439106e300ddb5875dcc441f77
17806 <17806709603@163.com>
2023-02-09 22:13:30 +0800

    elasticsearch repository

---
+++ mall-search/src/main/java/com/jitgur/mall/search/repository/EsProductRepository.java
@@ -0,0 +1,16 @@
+package com.jitgur.mall.search.repository;
+
+import com.jitgur.mall.search.domain.EsProduct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.awt.print.Pageable;
+
+/**
+ * elasticsearch repository
+ * Created by jitgur on 20230209
+ */
+public interface EsProductRepository extends ElasticsearchRepository<EsProduct, Long> {
+
+    Page<EsProduct> findByNameOrSubTitleOrKeywords(String name, String subTitle, String keywords, Pageable pageable);
+}
