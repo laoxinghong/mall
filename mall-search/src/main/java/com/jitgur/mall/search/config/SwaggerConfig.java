a76d12329a9fc98e160e081c891c5e65f5ff9bbe
17806 <17806709603@163.com>
2023-02-09 16:20:06 +0800

    swagger配置

---
+++ mall-search/src/main/java/com/jitgur/mall/search/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.jitgur.mall.search.config;
+
+import com.jitgur.mall.common.config.BaseSwaggerConfig;
+import com.jitgur.mall.common.domain.SwaggerProperties;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger配置
+ * Created  by jitgur on 20230209
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.jitgur.mall.search.controller")
+                .title("商品搜索系统")
+                .description("mall商品搜索系统相关文档")
+                .contactName("jitgur")
+                .version("1.0")
+                .enableSecurity(false)
+                .build();
+    }
+
+
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return generateBeanPostProcessor();
+    }
+
+}
