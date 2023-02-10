e15d825edf60cc7e5784c09e8f807c4082320c03
17806 <17806709603@163.com>
2023-02-10 21:55:49 +0800

    swagger config

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.jitgur.mall.portal.config;
+
+import com.jitgur.mall.common.config.BaseSwaggerConfig;
+import com.jitgur.mall.common.domain.SwaggerProperties;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger config
+ * Created by jitgur on 20230210
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.jitgur.mall.portal.controller")
+                .title("门户系统")
+                .description("前端调用相关接口文档")
+                .contactName("jitgur")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+
+
+    @Bean
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return generateBeanPostProcessor();
+    }
+
+}
