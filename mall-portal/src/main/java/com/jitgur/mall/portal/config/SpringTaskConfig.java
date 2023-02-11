ccabc21899aff625a7d20df9a426f56d61a0fac7
17806 <17806709603@163.com>
2023-02-11 22:12:34 +0800

    定时任务设置

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.jitgur.mall.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务设置
+ * Created by jitgur on 20230211
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
