66132654220d611e654599d34bbca6e46cb38287
17806 <17806709603@163.com>
2023-02-12 20:00:31 +0800

    security config

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/config/PortalSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jitgur.mall.portal.config;
+
+import com.jitgur.mall.portal.service.UmsMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * mall-security模块相关配置
+ * Created by jitgur on 20230212
+ */
+@Configuration
+public class PortalSecurityConfig {
+
+    @Autowired
+    private UmsMemberService memberService;
+
+
+    public UserDetailsService userDetailsService() {
+        return username -> memberService.loadUserByUsername(username);
+    }
+
+}
