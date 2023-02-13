84dfa8d3de36c86024f19f599ad1436720c7bd65
17806 <17806709603@163.com>
2023-02-13 18:16:25 +0800

    门户品牌管理

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/service/PortalBrandService.java
@@ -0,0 +1,31 @@
+package com.jitgur.mall.portal.service;
+
+import com.jitgur.mall.mbg.model.PmsBrand;
+import com.jitgur.mall.mbg.model.PmsProduct;
+
+import java.util.List;
+
+/**
+ * 门户品牌管理service
+ * Created by jitgur on 20230212
+ */
+public interface PortalBrandService {
+
+    /**
+     * 分页获取推荐品牌
+     */
+    List<PmsBrand> recommendBrandList(Integer pageNum, Integer pageSize);
+
+
+    /**
+     * 获取品牌详情
+     */
+    PmsBrand detail(Long brandId);
+
+
+    /**
+     * 分页获取品牌关联商品
+     */
+    List<PmsProduct> productList(Long brandId, Integer pageNum, Integer pageSize);
+
+}
---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/service/impl/PortalBrandServiceImpl.java
@@ -0,0 +1,51 @@
+package com.jitgur.mall.portal.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.jitgur.mall.mbg.mapper.PmsBrandMapper;
+import com.jitgur.mall.mbg.mapper.PmsProductMapper;
+import com.jitgur.mall.mbg.model.PmsBrand;
+import com.jitgur.mall.mbg.model.PmsProduct;
+import com.jitgur.mall.mbg.model.PmsProductExample;
+import com.jitgur.mall.portal.dao.SmsHomeDao;
+import com.jitgur.mall.portal.service.PortalBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 门户品牌管理service实现类
+ * Created by jitgur on 20230212
+ */
+@Service
+public class PortalBrandServiceImpl implements PortalBrandService {
+
+    @Autowired
+    private SmsHomeDao homeDao;
+    @Autowired
+    private PmsBrandMapper brandMapper;
+    @Autowired
+    private PmsProductMapper productMapper;
+
+
+    @Override
+    public List<PmsBrand> recommendBrandList(Integer pageNum, Integer pageSize) {
+        return homeDao.recommendBrandList((pageNum - 1) * pageSize, pageSize);
+    }
+
+
+    @Override
+    public PmsBrand detail(Long brandId) {
+        return brandMapper.selectByPrimaryKey(brandId);
+    }
+
+
+    @Override
+    public List<PmsProduct> productList(Long brandId, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        PmsProductExample example = new PmsProductExample();
+        example.createCriteria().andBrandIdEqualTo(brandId).andDeleteStatusEqualTo(0).andPublishStatusEqualTo(1);
+        return productMapper.selectByExample(example);
+    }
+
+}
