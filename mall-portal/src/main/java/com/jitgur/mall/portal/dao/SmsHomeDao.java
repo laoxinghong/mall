610ad3e8b709c5c0af964695c2518848f147fe67
17806 <17806709603@163.com>
2023-02-15 21:57:24 +0800

    首页内容管理

--- mall-portal/src/main/java/com/jitgur/mall/portal/dao/SmsHomeDao.java
+++ mall-portal/src/main/java/com/jitgur/mall/portal/dao/SmsHomeDao.java
@@ -1,6 +1,9 @@
 package com.jitgur.mall.portal.dao;
 
+import com.jitgur.mall.mbg.model.CmsSubject;
 import com.jitgur.mall.mbg.model.PmsBrand;
+import com.jitgur.mall.mbg.model.PmsProduct;
+import com.jitgur.mall.portal.domain.SmsFlashPromotionProduct;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -14,5 +17,30 @@ public interface SmsHomeDao {
     /**
      * 分页获取推荐品牌
      */
-    List<PmsBrand>recommendBrandList(@Param("offset")Integer offset,@Param("limit")Integer limit);
+    List<PmsBrand> recommendBrandList(@Param("offset") Integer offset, @Param("limit") Integer limit);
+
+
+    /**
+     * 首页热门商品推荐
+     */
+    List<PmsProduct> hotProductList(@Param("offset") Integer offset, @Param("limit") Integer limit);
+
+
+    /**
+     * 首页新品商品推荐
+     */
+    List<PmsProduct> newProductList(@Param("offset") Integer offset, @Param("limit") Integer limit);
+
+
+    /**
+     * 首页专题推荐
+     */
+    List<CmsSubject> homeSubjectList(@Param("offset") Integer offset, @Param("limit") Integer limit);
+
+
+    /**
+     * 获取秒杀商品信息
+     */
+    List<SmsFlashPromotionProduct> getFlashPromotionProductList(@Param("flashPromotionId") Long flashPromotionId, @Param("sessionId") Long sessionId);
+
 }
