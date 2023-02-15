255d6a917cd7c2a0678beab69cd1fa2950e0a2af
17806 <17806709603@163.com>
2023-02-15 21:18:24 +0800

    日期工具类

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/util/DateUtil.java
@@ -0,0 +1,37 @@
+package com.jitgur.mall.portal.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 日期工具类
+ * Created by jitgur on 20230215
+ */
+public class DateUtil {
+
+    /**
+     * 从Date对象中提取出日期部分
+     */
+    public static Date getDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
+
+
+    /**
+     * 从Date对象中提取出时间部分
+     */
+    public static Date getTime(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.YEAR, 1970);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
+    }
+
+}
