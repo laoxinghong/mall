0d8a4aac99df97deac2ee00359d523ff93cdb388
17806 <17806709603@163.com>
2023-02-16 22:14:53 +0800

    包含促销信息的购物车商品

---
+++ mall-portal/src/main/java/com/jitgur/mall/portal/domain/OmsCartPromotionItem.java
@@ -0,0 +1,29 @@
+package com.jitgur.mall.portal.domain;
+
+import com.jitgur.mall.mbg.model.OmsCartItem;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * 包含促销信息的购物车项目
+ * Created by jitgur on 20230216
+ */
+@Getter
+@Setter
+public class OmsCartPromotionItem extends OmsCartItem {
+
+    @ApiModelProperty("促销活动信息")
+    private String promotionMessage;
+    @ApiModelProperty("促销活动减去价格，针对单个商品")
+    private BigDecimal reducePrice;
+    @ApiModelProperty("真实库存：现有库存-锁定库存")
+    private Integer realStock;
+    @ApiModelProperty("购买商品赠送积分")
+    private Integer giftIntegration;
+    @ApiModelProperty("购买商品赠送成长值")
+    private Integer giftGrowth;
+
+}
