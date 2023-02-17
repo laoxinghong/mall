1dd3ac97baa7d06660c082e76b71f4a57c2f8830
17806 <17806709603@163.com>
2023-02-17 20:31:46 +0800

    优惠券管理

--- mall-portal/src/main/java/com/jitgur/mall/portal/dao/SmsCouponHistoryDao.java
+++ mall-portal/src/main/java/com/jitgur/mall/portal/dao/SmsCouponHistoryDao.java
@@ -1,6 +1,8 @@
 package com.jitgur.mall.portal.dao;
 
 import com.jitgur.mall.mbg.model.SmsCoupon;
+import com.jitgur.mall.portal.domain.SmsCouponHistoryDetail;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,5 +16,11 @@ public interface SmsCouponHistoryDao {
      * 获取当前用户优惠券列表
      */
     List<SmsCoupon> getCouponList(Long memberId, Integer userStatus);
+
+
+    /**
+     * 获取会员优惠券详情列表
+     */
+    List<SmsCouponHistoryDetail> getCouponHistoryDetailList(@Param("memberId") Long memberId);
 
 }
