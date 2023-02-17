1c107ebb1ae3d326d38addaf90ce1dbf047263f3
17806 <17806709603@163.com>
2023-02-17 16:46:23 +0800

    购物车管理

--- mall-portal/src/main/java/com/jitgur/mall/portal/service/OmsCartItemService.java
+++ mall-portal/src/main/java/com/jitgur/mall/portal/service/OmsCartItemService.java
@@ -1,6 +1,7 @@
 package com.jitgur.mall.portal.service;
 
 import com.jitgur.mall.mbg.model.OmsCartItem;
+import com.jitgur.mall.portal.domain.OmsCartProduct;
 import com.jitgur.mall.portal.domain.OmsCartPromotionItem;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,5 +30,35 @@ public interface OmsCartItemService {
      * 返回包含促销信息的购物车项目
      */
     List<OmsCartPromotionItem> listPromotion(Long memberId, List<Long> cartItemIdList);
+
+
+    /**
+     * 修改购物车商品购买数量
+     */
+    int updateQuantity(Long cartItemId, Long memberId, Integer quantity);
+
+
+    /**
+     * 批量删除购物车商品
+     */
+    int delete(Long memberId, List<Long> cartItemIdList);
+
+
+    /**
+     * 获取购物车中用于选择商品规格的商品信息
+     */
+    OmsCartProduct cartProduct(Long productId);
+
+
+    /**
+     * 修改购物车商品的规格
+     */
+    int updateAttr(OmsCartItem cartItem);
+
+
+    /**
+     * 清空购物车
+     */
+    void clear(Long memberId);
 
 }
--- mall-portal/src/main/java/com/jitgur/mall/portal/service/impl/OmsCartItemServiceImpl.java
+++ mall-portal/src/main/java/com/jitgur/mall/portal/service/impl/OmsCartItemServiceImpl.java
@@ -5,6 +5,8 @@ import com.jitgur.mall.mbg.mapper.OmsCartItemMapper;
 import com.jitgur.mall.mbg.model.OmsCartItem;
 import com.jitgur.mall.mbg.model.OmsCartItemExample;
 import com.jitgur.mall.mbg.model.UmsMember;
+import com.jitgur.mall.portal.dao.OmsCartItemDao;
+import com.jitgur.mall.portal.domain.OmsCartProduct;
 import com.jitgur.mall.portal.domain.OmsCartPromotionItem;
 import com.jitgur.mall.portal.service.OmsCartItemService;
 import com.jitgur.mall.portal.service.OmsCartPromotionService;
@@ -28,6 +30,8 @@ public class OmsCartItemServiceImpl implements OmsCartItemService {
     private UmsMemberService memberService;
     @Autowired
     private OmsCartPromotionService cartPromotionService;
+    @Autowired
+    private OmsCartItemDao cartItemDao;
 
 
     @Override
@@ -84,11 +88,66 @@ public class OmsCartItemServiceImpl implements OmsCartItemService {
                 .andMemberIdEqualTo(memberId)
                 .andDeleteStatusEqualTo(0);
         List<OmsCartItem> cartItemList = cartItemMapper.selectByExample(example);
-        if(CollUtil.isNotEmpty(cartItemList)){
+        if (CollUtil.isNotEmpty(cartItemList)) {
             return cartPromotionService.calcPromotion(cartItemList);
         }
         return null;
     }
 
+
+    @Override
+    public int updateQuantity(Long cartItemId, Long memberId, Integer quantity) {
+        OmsCartItem cartItem = new OmsCartItem();
+        cartItem.setQuantity(quantity);
+        cartItem.setModifyTime(new Date());
+        OmsCartItemExample example = new OmsCartItemExample();
+        example.createCriteria()
+                .andIdEqualTo(cartItemId)
+                .andMemberIdEqualTo(memberId)
+                .andDeleteStatusEqualTo(0);
+        return cartItemMapper.updateByExampleSelective(cartItem, example);
+    }
+
+
+    @Override
+    public int delete(Long memberId, List<Long> cartItemIdList) {
+        OmsCartItem cartItem = new OmsCartItem();
+        cartItem.setDeleteStatus(1);
+        cartItem.setModifyTime(new Date());
+        OmsCartItemExample example = new OmsCartItemExample();
+        example.createCriteria().andMemberIdEqualTo(memberId).andIdIn(cartItemIdList);
+        return cartItemMapper.updateByExampleSelective(cartItem, example);
+    }
+
+
+    @Override
+    public OmsCartProduct cartProduct(Long productId) {
+        return cartItemDao.cartProduct(productId);
+    }
+
+
+    @Override
+    public int updateAttr(OmsCartItem cartItem) {
+        OmsCartItem cartItem1 = new OmsCartItem();
+        cartItem1.setId(cartItem.getId());
+        cartItem1.setModifyTime(new Date());
+        cartItem1.setDeleteStatus(1);
+        // 设置原item为删除状态
+        cartItemMapper.updateByPrimaryKeySelective(cartItem1);
+        // 插入新item
+        cartItem.setId(null);
+        return add(cartItem);
+    }
+
+
+    @Override
+    public void clear(Long memberId) {
+        OmsCartItemExample example = new OmsCartItemExample();
+        example.createCriteria().andMemberIdEqualTo(memberId).andDeleteStatusEqualTo(0);
+        OmsCartItem cartItem = new OmsCartItem();
+        cartItem.setDeleteStatus(1);
+        cartItem.setModifyTime(new Date());
+        cartItemMapper.updateByExampleSelective(cartItem, example);
+    }
 
 }
