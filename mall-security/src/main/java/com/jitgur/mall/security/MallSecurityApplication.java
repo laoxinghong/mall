640f90ac5d4ffcf39bd4b6face51c9d6b1b20fdf
17806 <17806709603@163.com>
2023-02-21 20:30:29 +0800

    启动类

---
+++ mall-security/src/main/java/com/jitgur/mall/security/MallSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.jitgur.mall.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MallSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSecurityApplication.class, args);
+    }
+
+}
